1df0c3cea59fd25bea3cafaa70747519ed2c8c78
GianlorenzoDercole <johndercole01@gmail.com>
2022-11-06 13:22:26 -0800

    starter code for controllers

--- controllers/organizer.js
+++ controllers/organizer.js
@@ -1,1 +1,20 @@
 
+
+const db = require('../models')
+
+const router = require('express').Router()
+
+// GET /tasks -- index of tasks
+router.get('/', async (req, res) => {
+    try {
+        // find all tasks
+        const tasks = await db.Task.find({})
+        // send them to clinet
+        res.json(bounties)
+    } catch {
+        console.log(err)
+    }
+})
+
+
+module.exports = router
